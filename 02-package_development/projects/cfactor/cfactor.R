47758796a7fead5ce4b03993e7857d6f3707829a
lorwal <lorwal@me.com>
2016-08-27 09:20:36 +0300

    proper sorting if levels = NULL, cleaning options introduced but without effect

--- 02-package_development/projects/cfactor/cfactor.R
+++ 02-package_development/projects/cfactor/cfactor.R
@@ -1,26 +1,47 @@
-cfactor <- function(x, levels, labels = levels, ...) {
+cfactor <- function(x, levels = NULL, labels = levels, connector = c("to", "bis", "-"),
+                    cleaning = list(punct = c(from = ",'.", to = "."),
+                                    connector = list(from = c("to", "-", "bis"), to = " - "),
+                                    case = "upper",
+                                    numbersAsStrings = F),
+                    ...) {
         
         `%w/o%` <- function(x, y) x[!x %in% y]
         
+        # detect factor levels
+        if(is.null(levels)){
+                connector.ready <- paste0(connector, collapse = "|")
+                sep <- regexec(connector.ready, x)
+                start <- sapply(sep, "[[", 1) # extract start 
+                before <- substr(x, 1, start - 1)
+                before <- gsub("[[:space:]]", "", before)
+                finalorder <- order(as.numeric(gsub("[^[:digit:]]", "", before)))
+                
+                finallevels <- unique(x)[finalorder]
+        } else {
+                finallevels <- levels
+        }
+        
+        #  clean levels
+        if(!is.null(cleaning[["punct"]])) {
+                regex <- paste("[", cleaning[["punct"]][["from"]], "]", sep = "")
+        }
+        
+        
+        # check for empty / removed
         prior <- as.character(unique(x))
-        output<-factor(x, levels, labels, ...)
-        if(!setequal(prior, labels)) {
-                # labels that are not current names
-                if(!all(labels %in% prior)) {
-                        warning(paste("the following label(s) is / are empty:", c(labels %w/o% prior)),
+        output<-factor(x, levels = finallevels, labels = finallevels, ...)
+        if(!setequal(prior, levels(output))) {
+                # levels that are not current names
+                if(!all(levels(output) %in% prior)) {
+                        warning(paste("the following label(s) is / are empty: \n", paste(c(levels(output) %w/o% prior), collapse = "\n")),
                                 call. = F)
                 }
                 
-                # current names that don't become labels
-                if(!all(prior %in% labels)) {
-                        warning(paste("the following label(s) is / are removed:", prior[!(prior %in% labels)]),
+                # current names that don't become levels
+                if(!all(prior %in% levels(output))) {
+                        warning(paste("the following label(s) is / are removed: \n", paste(prior[!(prior %in% levels(output))], collapse = "\n")),
                                 call. = F)
                 }
-
-                # all old labels in new labels?
-                # all(labels %in% prior) # all new labels in old labels?
-                
-                
         }
         output
 }
---
+++ 02-package_development/projects/cfactor/test-cfactor.R
@@ -0,0 +1,2 @@
+money <- c("EUR 21 - EUR 22", "EUR 100 - 101", "EUR 1 - EUR 10", "EUR 11 bis EUR 20", "98 to 99")
+cfactor(money)
